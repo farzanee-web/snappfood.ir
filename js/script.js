9aae8403478917cff9359be6f0f5f4e6e5bc5a70
Farzane Rafiei <farzanerafiei@Farzanes-Laptop.local>
2024-11-11 21:10:09 +0330

    Adding Snappfood-edited Project

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function() {
+
+    $('.show-modal').on('click', function() {
+        $('#locModal').modal('show');
+    });
+
+    $('.close-modal').on('click', function() {
+        $('#locModal').modal('hide');
+    });
+});
+
+
+
